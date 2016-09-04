604868a7047d742dbda1b6b56a2dc34fbcbe54ec
Juxtar <julianema6@gmail.com>
2016-09-04 17:28:01 -0300

    Agregada validación de horarios inicio/fin

--- src/js/directives/scheduleForm.js
+++ src/js/directives/scheduleForm.js
@@ -5,18 +5,35 @@ directive("scheduleForm", function(){
         templateUrl: 'templates/scheduleForm.html',
         link: function(scope, element, attr){
             scope.num = scope.scheduleNum;
+            scope.timeEnd = scope.timeEnd[scope.num];
+            scope.timeInit = scope.timeInit[scope.num];
+            scope.selected = scope.selected[scope.num];
+
             scope.removeScheduleForm = function() {
                 element.remove();
-                delete scope.timeInit[scope.num];
-                delete scope.timeEnd[scope.num];
-                delete scope.selected[scope.num];
+                delete scope.timeInit;
+                delete scope.timeEnd;
+                delete scope.selected;
                 scope.$destroy();
             }
-            
+
             function validateDays(){
-                scope.scheduleForm.days.$setValidity("required", !!scope.selected[scope.num].length);
+                scope.scheduleForm.errors.$setValidity("required", !!scope.selected.length);
             }
-          
+
+            function setTimeError(){
+                scope.errors = !(scope.scheduleForm.timeEnd.$invalid ||
+                                    scope.scheduleForm.timeInit.$invalid) &&
+                                (scope.timeEnd <= scope.timeInit);
+            }
+
+            scope.$watch('timeEnd', setTimeError);
+            scope.$watch('timeInit', setTimeError);
+
+            scope.scheduleForm.errors.$validators.validTime = function(modelValue){
+                return !modelValue;
+            }
+
             scope.toggle = function (item, list) {
                 var idx = list.indexOf(item);
                 if (idx > -1) {
